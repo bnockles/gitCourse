9be5cccac9ad9c4bc6b073b7cea1c43e8216d68a
Huo <Huo@MyPC>
2017-05-15 18:40:12 -0400

    Commit

---
+++ GithubCourse/src/customRooms/HuoRoom.java
@@ -0,0 +1,40 @@
+package customRooms;
+
+import caveExplorer.CaveRoom;
+
+public class HuoRoom extends CaveRoom {
+
+	public HuoRoom() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getDescription() {
+		
+		return "You are now in Huo's room. Welcome!\nPress\'J\' to ask for dinner.\nPress\'K\'to say \"hi\".";
+	}
+
+	@Override
+	public String getActionJOutput() {
+		
+		return "Be our guest~ be our guest~Put our service to...(singing, dancing)";
+	}
+
+	@Override
+	public String getActionKOutput() {
+		return "Hi";
+	}
+
+	@Override
+	public String getActionJDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getActionKDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
