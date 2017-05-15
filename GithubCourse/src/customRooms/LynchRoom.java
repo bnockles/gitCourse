9c02c435e390dc9162916f08bcd8b79e3d0a0053
Chris <Chris@Lynch>
2017-05-15 19:04:36 -0400

    here it is - Chris L

---
+++ GithubCourse/src/customRooms/LynchRoom.java
@@ -0,0 +1,41 @@
+package customRooms;
+
+import caveExplorer.CaveRoom;
+
+public class LynchRoom extends CaveRoom {
+
+	public LynchRoom() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getActionJOutput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getActionKOutput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getActionJDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getActionKDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
