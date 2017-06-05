0e94670eb9d57167993bd7698da79738103dbf71
Chris <Chris@Lynch>
2017-06-05 18:55:30 -0400

    6/5 Chris L.

--- GithubCourse/src/customRooms/LynchRoom.java
+++ GithubCourse/src/customRooms/LynchRoom.java
@@ -11,7 +11,7 @@ public class LynchRoom extends CaveRoom {
 	@Override
 	public String getDescription() {
 		// TODO Auto-generated method stub
-		return "Hello2";
+		return "Hello, This is Chris L.";
 	}
 
 	@Override
