f3f4655db29128764cf4a4f3a2e8496fe39f6fff
Haddad <ms.haddad@fmhsnyc.org>
2017-06-05 18:23:19 -0400

    trying out a room

--- GithubCourse/src/caveExplorer/CaveExplorer.java
+++ GithubCourse/src/caveExplorer/CaveExplorer.java
@@ -68,7 +68,8 @@ public class CaveExplorer {
 		corrineRoom = (CorinneRoom) assignRoom(new CorinneRoom(),3,1);
 		andrewRoom = (AndrewRoom) assignRoom(new AndrewRoom(),3,2);
 
-
+//NOTE: for testing, put desired room here
+		caves[0][1]=christaRoom;
 		
 		
 		currentRoom = caves[1][1];
